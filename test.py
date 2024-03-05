e78bc231917838f19a468f01ce5d2aff3153e7e1
Anthony Tong <atong28.usa@gmail.com>
2024-03-04 22:55:37 -0800

    Delete old main files

--- test.py
+++
@@ -1,8 +0,0 @@
-import re
-
-regex_pattern = "(.*) (.*) (.*) (.*)"  # Using a non-greedy match to capture each .* independently
-text = "Abyssinian_100 1 1 1"
-
-matches = re.findall(regex_pattern, text)
-
-print(matches)
--- utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def assert_file_exists(path):
-    pass
-
-def assert_folder_exists(path):
-    pass
-
