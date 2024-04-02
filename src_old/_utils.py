5460c2343d830c1a99bf96d8e9edec44e6457de7
Anthony Tong <atong28.usa@gmail.com>
2024-04-02 11:07:42 -0700

    Delete old src

--- src_old/_utils.py
+++
@@ -1,7 +0,0 @@
-def union(item: list[object] | object) -> list[object]:
-    '''
-    Returns the item in a list format if not already in a list format.
-    '''
-    if not isinstance(item, list):
-        item = [ item ]
-    return item
